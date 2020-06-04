3125d695a47e97f5f52e01310d04ef1e450def59
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-04 15:04:54 +0200

    Refactor - move image loader to seperate module

---
+++ public/js/loaders.js
@@ -0,0 +1,10 @@
+export function loadImage(url) {
+  return new Promise((resolve) => {
+    //
+    const image = new Image();
+    image.addEventListener('load', () => {
+      resolve(image);
+    });
+    image.src = url;
+  });
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,5 @@
 import SpriteSheet from './SpriteSheet.js';
-function loadImage(url) {
-  return new Promise((resolve) => {
-    //
-    const image = new Image();
-    image.addEventListener('load', () => {
-      resolve(image);
-    });
-    image.src = url;
-  });
-}
+import { loadImage } from './loaders.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
