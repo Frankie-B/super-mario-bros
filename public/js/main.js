ad1b4c0c51475992f9c345d9f6605544dbd12b3c
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-04 14:12:46 +0200

    Add function to draw tiles on canvas

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,19 @@
+function loadImage(url) {
+  return new Promise((resolve) => {
+    //
+    const image = new Image();
+    image.addEventListener('load', () => {
+      resolve(image);
+    });
+    image.src = url;
+  });
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 context.fillRect(0, 0, 50, 50);
+
+loadImage('/img/tiles.png').then((image) => {
+  context.drawImage(image, 0, 0);
+});
