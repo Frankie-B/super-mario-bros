044ea151adcadb715f37114472074f685c857fd6
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-04 15:25:18 +0200

    Add json level builder

--- public/js/main.js
+++ public/js/main.js
@@ -7,13 +7,21 @@ const context = canvas.getContext('2d');
 context.fillRect(0, 0, 50, 50);
 
 loadImage('/img/tiles.png').then((image) => {
-  // Create a sprite API to hand loading of tiles
   const sprites = new SpriteSheet(image, 16, 16);
   sprites.define('ground', 0, 0);
   sprites.define('sky', 3, 23);
+
+  // draw sky
   for (let x = 0; x < 25; x++) {
     for (let y = 0; y < 14; y++) {
       sprites.drawTile('sky', context, x, y);
+    }
+  }
+
+  // draw ground
+  for (let x = 0; x < 25; x++) {
+    for (let y = 12; y < 14; y++) {
+      sprites.drawTile('ground', context, x, y);
     }
   }
 });
