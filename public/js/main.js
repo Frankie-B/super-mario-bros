6213eeea719921b0800a6335dfa3ca6796e8cfcc
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-06 09:45:06 +0200

    Optimize loading of background sprites and level

--- public/js/main.js
+++ public/js/main.js
@@ -12,17 +12,24 @@ function drawBackground(background, context, sprites) {
   });
 }
 
+function loadBackgroundSprites() {
+  return loadImage('/img/tiles.png').then((image) => {
+    console.log('Image loaded', image);
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground', 0, 0);
+    sprites.define('sky', 3, 23);
+    return sprites;
+  });
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-loadImage('/img/tiles.png').then((image) => {
-  const sprites = new SpriteSheet(image, 16, 16);
-  sprites.define('ground', 0, 0);
-  sprites.define('sky', 3, 23);
-
-  loadLevel('1-1').then((level) => {
+Promise.all([loadBackgroundSprites(), loadLevel('1-1')]).then(
+  ([sprites, level]) => {
+    console.log('level loaded', level);
     level.backgrounds.forEach((background) => {
       drawBackground(background, context, sprites);
     });
-  });
-});
+  }
+);
