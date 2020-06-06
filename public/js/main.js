dca270d92ab0e4bd48cd6fbd80ecfaebdeef7293
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-06 22:43:28 +0200

    Add refactord stable timer for Mario jump

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 import Compositor from './Compositor.js';
-import Entity from './Entity.js';
+import Timer from './Timer.js';
 import { loadLevel } from './loaders.js';
 import { createMario } from './entities.js';
 import { loadBackgroundSprites } from './sprites.js';
@@ -16,7 +16,7 @@ Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')]).then(
       backgroundSprites
     );
 
-    // comp.layers.push(backgroundLayer);
+    comp.layers.push(backgroundLayer);
 
     const gravity = 30;
     mario.pos.set(64, 180);
@@ -25,29 +25,14 @@ Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')]).then(
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
 
-    const deltaTime = 1 / 60;
-    // accumulator pattern for time
-    let accumulatedTime = 0;
-    let lastTime = 0;
+    const timer = new Timer(1 / 60);
 
-    function updatePos(time) {
-      accumulatedTime += (time - lastTime) / 1000;
+    timer.update = function updatePos(deltaTime) {
+      comp.draw(context);
+      mario.update(deltaTime);
+      mario.velocity.y += gravity;
+    };
 
-      while (accumulatedTime > deltaTime) {
-        comp.draw(context);
-        mario.update(deltaTime);
-        console.log(mario.pos);
-        mario.velocity.y += gravity;
-
-        accumulatedTime -= deltaTime;
-      }
-      // requestAnimationFrame(updatePos);
-
-      setTimeout(updatePos, 1000 / 60, performance.now());
-
-      lastTime = time;
-    }
-
-    updatePos(0);
+    timer.start();
   }
 );
