6599fde507edfd529b3a0cfbc2d6e0824146273b
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-06 11:23:17 +0200

    Refactor - move compoisitor to its own module

--- public/js/main.js
+++ public/js/main.js
@@ -15,18 +15,6 @@ function drawBackground(background, context, sprites) {
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-class Compositor {
-  constructor() {
-    this.layers = [];
-  }
-
-  draw(context) {
-    this.layers.forEach((layer) => {
-      layer(context);
-    });
-  }
-}
-
 function createBackgroundLayer(backgrounds, sprites) {
   const buffer = document.createElement('canvas');
   buffer.width = 256;
