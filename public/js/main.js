b44828dc8fcbfc1259c29ee9e340f39c362f6f06
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-06 10:34:30 +0200

    Refactor - move sprite loaders to sprites.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-import SpriteSheet from './SpriteSheet.js';
-import { loadImage, loadLevel } from './loaders.js';
+import { loadLevel } from './loaders.js';
+import { loadMarioSprite, loadBackgroundSprites } from './sprites.js';
 
 function drawBackground(background, context, sprites) {
   background.ranges.forEach(([x1, x2, y1, y2]) => {
@@ -9,23 +9,6 @@ function drawBackground(background, context, sprites) {
         sprites.drawTile(background.tile, context, x, y);
       }
     }
-  });
-}
-
-function loadMarioSprite() {
-  return loadImage('/img/characters.gif').then((image) => {
-    const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define('idle', 276, 44, 16, 16);
-    return sprites;
-  });
-}
-
-function loadBackgroundSprites() {
-  return loadImage('/img/tiles.png').then((image) => {
-    const sprites = new SpriteSheet(image, 16, 16);
-    sprites.defineTile('ground', 0, 0);
-    sprites.defineTile('sky', 3, 23);
-    return sprites;
   });
 }
 
