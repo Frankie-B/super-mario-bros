6a8b4bfced0d3a271d243667c460d240f9387b73
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-09 10:23:23 +0200

    Add function to find tile collion range

--- public/js/TileResolver.js
+++ public/js/TileResolver.js
@@ -8,6 +8,17 @@ export default class TileResolver {
     return Math.floor(pos / this.tileSize);
   }
 
+  toIndexRange(pos1, pos2) {
+    const pMax = Math.ceil(pos2 / this.tileSize) * this.tileSize;
+    const range = [];
+    let pos = pos1;
+    do {
+      range.push(this.toIndex(pos));
+      pos += this.tileSize;
+    } while (pos < pMax);
+    return range;
+  }
+
   getByIndex(indexX, indexY) {
     const tile = this.matrix.get(indexX, indexY);
     if (tile) {
@@ -21,7 +32,21 @@ export default class TileResolver {
     }
   }
 
-  matchByPosition(posX, posY) {
+  searchByPosition(posX, posY) {
     return this.getByIndex(this.toIndex(posX), this.toIndex(posY));
+  }
+
+  searchByRange(x1, x2, y1, y2) {
+    const matches = [];
+    this.toIndexRange(x1, x2).forEach((indexX) => {
+      this.toIndexRange(y1, y2).forEach((indexY) => {
+        const match = this.getByIndex(indexX, indexY);
+        if (match) {
+          matches.push(match);
+        }
+      });
+    });
+
+    return matches;
   }
 }
