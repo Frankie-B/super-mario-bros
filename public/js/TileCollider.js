1994e870b66de5531e6b4f9ed0fe635c81d059c1
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-09 12:04:18 +0200

    Optimize collision search ranges

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -6,9 +6,18 @@ export default class TileCollider {
   }
 
   checkX(entity) {
+    let x;
+    if (entity.vel.x > 0) {
+      x = entity.pos.x + entity.size.x;
+    } else if (entity.vel.x < 0) {
+      x = entity.pos.x;
+    } else {
+      return;
+    }
+
     const matches = this.tiles.searchByRange(
-      entity.pos.x,
-      entity.pos.x + entity.size.x,
+      x,
+      x,
       entity.pos.y,
       entity.pos.y + entity.size.y
     );
@@ -33,11 +42,20 @@ export default class TileCollider {
   }
 
   checkY(entity) {
+    let y;
+    if (entity.vel.y > 0) {
+      y = entity.pos.y + entity.size.y;
+    } else if (entity.vel.y < 0) {
+      y = entity.pos.y;
+    } else {
+      return;
+    }
+
     const matches = this.tiles.searchByRange(
       entity.pos.x,
       entity.pos.x + entity.size.x,
-      entity.pos.y,
-      entity.pos.y + entity.size.y
+      y,
+      y
     );
 
     matches.forEach((match) => {
