7e5694887e782c3e9487fdbf7682c25aa32b6209
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-11 17:32:43 +0200

    Create frame router function for mario sprite

--- public/js/entities.js
+++ public/js/entities.js
@@ -5,7 +5,6 @@ import Go from './traits/Go.js';
 
 export function createMario() {
   return loadSpriteSheet('mario').then((sprite) => {
-    console.log(sprite);
     const mario = new Entity();
 
     mario.size.set(14, 16);
@@ -14,8 +13,13 @@ export function createMario() {
 
     mario.addTrait(new Jump());
 
+    // mario frame router
+    function routeFrame(mario) {
+      return 'idle';
+    }
+
     mario.draw = function drawMario(context) {
-      sprite.draw('idle', context, 0, 0);
+      sprite.draw(routeFrame(this), context, 0, 0);
     };
 
     return mario;
