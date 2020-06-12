701d68736cb07f7f7683b533b5101be0be243942
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-12 11:39:29 +0200

    Add break animation to mario run turn dir

--- public/js/entities.js
+++ public/js/entities.js
@@ -19,6 +19,12 @@ export function createMario() {
     // mario frame router
     function routeFrame(mario) {
       if (mario.go.distance > 0) {
+        if (
+          (mario.vel.x > 0 && mario.go.dir < 0) ||
+          (mario.vel.x < 0 && mario.go.dir > 0)
+        ) {
+          return 'break';
+        }
         return runAnim(mario.go.distance);
       }
       return 'idle';
