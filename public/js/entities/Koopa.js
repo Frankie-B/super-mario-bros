f615dc1273fc5b96e8f9c7d73b41f7220c9f6002
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-20 15:25:33 +0200

    Decouple  pendulum walk and create new trait

--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -1,4 +1,5 @@
 import Entity, { Sides } from '../Entity.js';
+import PendulumWalk from '../traits/PendulumWalk.js';
 import { loadSpriteSheet } from '../loaders.js';
 
 export function loadKoopa() {
@@ -18,18 +19,7 @@ function createKoopaFactory(sprite) {
     const koopa = new Entity();
     koopa.size.set(16, 16);
 
-    koopa.addTrait({
-      NAME: 'walk',
-      speed: -30,
-      obstruct(koopa, side) {
-        if (side === Sides.LEFT || side === Sides.RIGHT) {
-          this.speed = -this.speed;
-        }
-      },
-      update(koopa) {
-        koopa.vel.x = this.speed;
-      },
-    });
+    koopa.addTrait(new PendulumWalk());
 
     koopa.draw = drawKoopa;
 
