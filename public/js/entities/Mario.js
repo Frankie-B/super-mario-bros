3a33958bdcc52701f02e5a0a3bda20581c2c3248
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-20 13:52:30 +0200

    Refactor - create mario into a factory

--- public/js/entities/Mario.js
+++ public/js/entities/Mario.js
@@ -2,7 +2,6 @@ import Entity from '../Entity.js';
 import Go from '../traits/Go.js';
 import Jump from '../traits/Jump.js';
 import { loadSpriteSheet } from '../loaders.js';
-import { createAnim } from '../anim.js';
 
 const SLOW_DRAG = 1 / 1000;
 const FAST_DRAG = 1 / 5000;
@@ -12,41 +11,44 @@ export function loadMario() {
 }
 
 function createMarioFactory(sprite) {
+  const runAnim = sprite.animations.get('run');
+
+  function routeFrame(mario) {
+    if (mario.jump.falling) {
+      return 'jump';
+    }
+    if (mario.go.distance > 0) {
+      if (
+        (mario.vel.x > 0 && mario.go.dir < 0) ||
+        (mario.vel.x < 0 && mario.go.dir > 0)
+      ) {
+        return 'break';
+      }
+      return runAnim(mario.go.distance);
+    }
+    return 'idle';
+  }
+
+  function setTurboState(turboOn) {
+    this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
+  }
+
+  function drawMario(context) {
+    sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
+  }
+
   return function createMario() {
     const mario = new Entity();
 
     mario.size.set(14, 16);
 
     mario.addTrait(new Go());
-    mario.go.dragFactor = SLOW_DRAG;
 
     mario.addTrait(new Jump());
 
-    mario.turbo = function setTurboState(turboOn) {
-      this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
-    };
-
-    const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6);
-
-    function routeFrame(mario) {
-      if (mario.jump.falling) {
-        return 'jump';
-      }
-      if (mario.go.distance > 0) {
-        if (
-          (mario.vel.x > 0 && mario.go.dir < 0) ||
-          (mario.vel.x < 0 && mario.go.dir > 0)
-        ) {
-          return 'break';
-        }
-        return runAnim(mario.go.distance);
-      }
-      return 'idle';
-    }
-
-    mario.draw = function drawMario(context) {
-      sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
-    };
+    mario.turbo = setTurboState;
+    mario.draw = drawMario;
+    mario.turbo(false);
 
     return mario;
   };
