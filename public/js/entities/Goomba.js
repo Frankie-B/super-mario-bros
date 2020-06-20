2afa48f4ff45fdd9b3d5b4982dbb6c26032cb389
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-20 15:52:30 +0200

    Add offset to collision layer

--- public/js/entities/Goomba.js
+++ public/js/entities/Goomba.js
@@ -1,4 +1,5 @@
 import Entity, { Sides } from '../Entity.js';
+import PendulumWalk from '../traits/PendulumWalk.js';
 import { loadSpriteSheet } from '../loaders.js';
 
 export function loadGoomba() {
@@ -18,18 +19,7 @@ function createGoombaFactory(sprite) {
     const goomba = new Entity();
     goomba.size.set(16, 16);
 
-    goomba.addTrait({
-      NAME: 'walk',
-      speed: -30,
-      obstruct(goomba, side) {
-        if (side === Sides.LEFT || side === Sides.RIGHT) {
-          this.speed = -this.speed;
-        }
-      },
-      update(goomba) {
-        goomba.vel.x = this.speed;
-      },
-    });
+    goomba.addTrait(new PendulumWalk());
 
     goomba.draw = drawGoomba;
 
