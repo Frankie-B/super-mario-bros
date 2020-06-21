2157f7a292483c9f684685bafc601d031aa97e4f
Francis Bukenya <frankie.bukenya@gmail.com>
2020-06-21 23:16:37 +0200

    Sunday night WIP

--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -1,4 +1,5 @@
-import Entity, { Sides } from '../Entity.js';
+import Entity, { Sides, Trait } from '../Entity.js';
+import Killable from '../traits/Killable.js';
 import PendulumWalk from '../traits/PendulumWalk.js';
 import { loadSpriteSheet } from '../loaders.js';
 
@@ -6,11 +7,72 @@ export function loadKoopa() {
   return loadSpriteSheet('koopa').then(createKoopaFactory);
 }
 
+const STATE_WALKING = Symbol('walking');
+const STATE_HIDING = Symbol('hiding');
+
+class Behavior extends Trait {
+  constructor() {
+    super('behavior');
+    this.hideTime = 0;
+
+    this.state = STATE_WALKING;
+  }
+
+  collides(us, them) {
+    if (us.killable.dead) {
+      return;
+    }
+
+    if (them.stomper) {
+      if (them.vel.y > us.vel.y) {
+        this.handleStomp(us, them);
+        them.stomper.bounce();
+      } else {
+        them.killable.kill();
+      }
+    }
+  }
+
+  handleStomp(us, them) {
+    if (this.state === STATE_WALKING) {
+      this.hide(us);
+    }
+  }
+
+  hide(us) {
+    us.vel.x = 0;
+    us.pendulumWalk.speed = 0;
+    this.hideTime = 0;
+    this.state = STATE_HIDING;
+  }
+
+  unhide(us) {
+    us.pendulumWalk.speed = 100;
+    this.state = STATE_WALKING;
+  }
+
+  update(us, deltaTime) {
+    if (this.state === STATE_HIDING) {
+      this.hideTime += deltaTime;
+      if (this.hideTime > this.hideDuration) {
+        this.unhide(us);
+      }
+    }
+  }
+}
+
 function createKoopaFactory(sprite) {
   const walkAnim = sprite.animations.get('walk');
 
+  function routeAnim(koopa) {
+    if (koopa.behavior.state === STATE_HIDING) {
+      return 'hiding';
+    }
+    return walkAnim(koopa.lifetime);
+  }
+
   function drawKoopa(context) {
-    sprite.draw(walkAnim(this.lifetime), context, 0, 0, this.vel.x < 0);
+    sprite.draw(routeAnim(this), context, 0, 0, this.vel.x < 0);
   }
 
   return function createKoopa() {
@@ -19,6 +81,8 @@ function createKoopaFactory(sprite) {
     koopa.offset.y = 8;
 
     koopa.addTrait(new PendulumWalk());
+    koopa.addTrait(new Killable());
+    koopa.addTrait(new Behavior());
 
     koopa.draw = drawKoopa;
 
