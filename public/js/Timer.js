76c939a1a37fb95b125a18d8f0a2d00dacceca63
Francis Bukenya <frankie.bukenya@gmail.com>
2020-07-01 17:36:12 +0200

    Fix Timer.js

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -1,31 +1,33 @@
 export default class Timer {
-    constructor(deltaTime = 1/60) {
-        let accumulatedTime = 0;
-        let lastTime = 0;
+  constructor(deltaTime = 1 / 60) {
+    let accumulatedTime = 0;
+    let lastTime = null;
 
-        this.updateProxy = (time) => {
-            accumulatedTime += (time - lastTime) / 1000;
+    this.updateProxy = (time) => {
+      if (lastTime) {
+        accumulatedTime += (time - lastTime) / 1000;
 
-            if (accumulatedTime > 1) {
-                accumulatedTime = 1;
-            }
-
-            while (accumulatedTime > deltaTime) {
-                this.update(deltaTime);
-                accumulatedTime -= deltaTime;
-            }
-
-            lastTime = time;
-
-            this.enqueue();
+        if (accumulatedTime > 1) {
+          accumulatedTime = 1;
         }
-    }
 
-    enqueue() {
-        requestAnimationFrame(this.updateProxy);
-    }
+        while (accumulatedTime > deltaTime) {
+          this.update(deltaTime);
+          accumulatedTime -= deltaTime;
+        }
+      }
 
-    start() {
-        this.enqueue();
-    }
+      lastTime = time;
+
+      this.enqueue();
+    };
+  }
+
+  enqueue() {
+    requestAnimationFrame(this.updateProxy);
+  }
+
+  start() {
+    this.enqueue();
+  }
 }
